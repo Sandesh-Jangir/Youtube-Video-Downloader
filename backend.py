159a541c23192d38b360511029a88bdbe3fdc21b
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-08-09 16:55:35 +0530

    Made it functionable

--- backend.py
+++ backend.py
@@ -1,4 +1,10 @@
 import eel
+from pytube import YouTube
 eel.init('web')
-
+@eel.expose
+def download_video(url):
+    yt = YouTube(url)
+    video = yt.streams.filter(res="720p").first()
+    video.download()
+    
 eel.start("main.html")
